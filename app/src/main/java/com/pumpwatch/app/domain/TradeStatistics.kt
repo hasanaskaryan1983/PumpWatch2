f14b25e6fa451c1419c8cacb0d3a31236eaf19c0
hasanaskaryan1983 <hasan.askaryan1983@gmail.com>
2026-08-21 03:14:44 +0330

    Update TradeStatistics.kt

--- app/src/main/java/com/pumpwatch/app/domain/TradeStatistics.kt
+++ app/src/main/java/com/pumpwatch/app/domain/TradeStatistics.kt
@@ -1,1 +1,15 @@
 
+package com.pumpwatch.app.domain
+
+data class TradeStatistics(
+    val totalTrades: Int,
+    val winningTrades: Int,
+    val losingTrades: Int,
+    val winRatePercent: Double,
+    val totalProfit: Double,
+    val averageWinPercent: Double,
+    val averageLossPercent: Double,
+    val largestWin: Double,
+    val largestLoss: Double,
+    val profitFactor: Double
+)
