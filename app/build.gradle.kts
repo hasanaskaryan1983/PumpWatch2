46572db1782ebf4f89fdd4a716f0f329f0a5aea9
hasanaskaryan1983 <hasan.askaryan1983@gmail.com>
2026-08-22 19:46:34 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24"
 }
 
 android {
@@ -74,21 +73,13 @@ dependencies {
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.0")
     
-    // DataStore
+    // DataStore (جایگزین Room)
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Retrofit + OkHttp
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    // Room Database
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-
-    // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
 
     // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
